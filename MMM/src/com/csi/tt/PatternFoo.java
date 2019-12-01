348ffd41cbdc3105d2dffa787aca8380b05c3c63
Namrata-s-lang <nmrt1947@gmail.com>
2019-12-01 13:09:38 +0530

    Changes in syso

---
+++ MMM/src/com/csi/tt/PatternFoo.java
@@ -0,0 +1,18 @@
+package com.csi.tt;
+
+public class PatternFoo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hi");
+		 int i,j;
+		 for(i=0;i<=3;i++){
+			 System.out.print("\n * " );
+		 
+			 for(j=0;j<=i;j++){
+				 System.out.print(" * ");
+			 }System.out.println();
+		 }
+	}
+
+}
