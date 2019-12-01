a7d48263930bf31226257815a89d3fac2847dc4a
Namrata-s-lang <nmrt1947@gmail.com>
2019-12-01 13:29:59 +0530

    changes in test

---
+++ TestProject/src/com/csi/test/Test.java
@@ -0,0 +1,9 @@
+package com.csi.test;
+
+public class Test {
+public static void main(String[] args) {
+	System.out.println("Credit Systems India");
+	System.out.println("Nigdi");
+	System.out.println("Pune");
+}
+}
